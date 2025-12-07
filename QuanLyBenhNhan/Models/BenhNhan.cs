13f337edb99f05b7dde2dba2aeb609f6a651518c
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2025-12-07 14:34:43 +0700

    add all service

---
+++ QuanLyBenhNhan/Models/BenhNhan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyBenhNhan.Models;
+
+public partial class BenhNhan
+{
+    public Guid Id { get; set; }
+
+    public string? HoTen { get; set; }
+
+    public DateOnly? NgaySinh { get; set; }
+
+    public string? GioiTinh { get; set; }
+
+    public string? DiaChi { get; set; }
+
+    public string? SoTheBaoHiem { get; set; }
+
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+
+    public virtual ICollection<NhapVien> NhapViens { get; set; } = new List<NhapVien>();
+}
---
+++ QuanLyBenhNhan/Models/DichVuDieuTri.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyBenhNhan.Models;
+
+public partial class DichVuDieuTri
+{
+    public Guid Id { get; set; }
+
+    public Guid? NhapVienId { get; set; }
+
+    public string? LoaiDichVu { get; set; }
+
+    public int? SoLuong { get; set; }
+
+    public DateTime? Ngay { get; set; }
+
+    public Guid? BacSiId { get; set; }
+
+    public Guid? DieuDuongId { get; set; }
+
+    public virtual BacSi? BacSi { get; set; }
+
+    public virtual DieuDuong? DieuDuong { get; set; }
+
+    public virtual NhapVien? NhapVien { get; set; }
+}

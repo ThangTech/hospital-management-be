127fa1c19c2de7cd0d1cd65141ccb551f383e014
Sondzzz1 <phanduyson123456789@gmail.com>
2026-01-04 16:24:06 +0700

    bo sung update and delete giuongbenh

---
+++ YtaService/DTO/GiuongUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YtaService.DTO
+{
+    public class GiuongUpdateDTO
+    {
+        public Guid Id { get; set; }        // Bắt buộc là GUID
+        public Guid KhoaId { get; set; }    // Bắt buộc là GUID
+        public string LoaiGiuong { get; set; }
+        public string TrangThai { get; set; }
+        public decimal GiaTien { get; set; }
+        public string TenGiuong { get; set; }
+    }
+}
